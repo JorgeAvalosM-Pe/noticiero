1c5ff46868164a3e3da10c653b4c79b462b94bc4
JorgeAvalosM-Pe <jorgeavalos71237099@gmail.com>
2025-10-28 19:02:38 -0500

    Actualizo el título de la página principal

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+ "titulo" => "Descubren un nuevo exoplaneta similar a la Tierra",
+  "autor" => "Agencia Espacial Europea (ESA)",
+  "resumen" => "Astrónomos han identificado un planeta con condiciones muy parecidas a las de la Tierra, ubicado a 120 años luz de distancia.",
+  "link" => "https://www.elmundo.es/ciencia-y-salud/2025/10/28/descubren-exoplaneta-tierra.html"
 ];
 ?>
